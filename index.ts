83edf1d994ea4d10f45b0fb88b2035552d7f3a91
Megan Tan <meganytan@gmail.com>
2025-03-27 01:25:13 -0500

    test change for deployment

--- index.ts
+++ index.ts
@@ -2,9 +2,10 @@ import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
 
 // Create a GCP resource (Storage Bucket)
-const bucket = new gcp.storage.Bucket("my-bucket", {
+const bucket = new gcp.storage.Bucket("my-bucket-1", {
     location: "US"
 });
 
 // Export the DNS name of the bucket
 export const bucketName = bucket.url;
+//
